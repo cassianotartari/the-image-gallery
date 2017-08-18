54e6b65960e45c34b8f392a756d54f40491281f9
Cassiano Tartari <cassianotartari@gmail.com>
2017-08-18 13:28:46 -0300

    Add basic image upload and viewing to our application

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,6 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new Silex\Application();
 
 $app['debug'] = true;
+$app['upload_folder'] = __DIR__ . '/uploads';
 
 return $app;
--- web/index.php
+++ web/index.php
@@ -4,10 +4,71 @@ require __DIR__ . '/../bootstrap.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+// our upload form action
+$app->get( '/upload/', function() {
+    $upload_form = 
+<<<EOF
+    <html>
+    <body>
+    <form enctype="multipart/form-data" action="" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="52428800" />
+    Choose a Photo:
+    <br><br>
+    <input name="image" type="file" />
+    <br><br>
+    <input type="submit" value="Upload Photo" />
+    </form>
+    </body>
+    </html>
+EOF;
+            
+    return $upload_form;
+});
+
 
 // Declare our primary action
 $app->get('/', function() use ($app) {
-    return new Response('Mr Watson, come here, I want to see you.', 200);
+    $images = glob($app['upload_folder'] . '/img*');
+    $out = '<html><body>';
+
+    foreach($images as $img) {
+        $out .= '<img src="/img/' . basename($img) . '"><br><br>';
+    }
+
+    $out .= '<a href="/upload/">Upload Photos &raquo;</a>';
+    $out .= '</body></html>';
+
+    return $out;
 });
+
+$app->post('/upload/', function(Request $request) use ($app) {
+    $files = $request->files;
+
+    if ($files->has('image')) {
+        $image = $files->get('image');
+        $image->move(
+            $app['upload_folder'],
+            tempnam($app['upload_folder'], 'img_')
+        );
+    }
+
+    // Redirect the user to the gallery page
+    return new RedirectResponse('/', 302);
+});
+
+$app->get('/img/{name}', function($name, Request $request) use ($app) {
+    $path = realpath($app['upload_folder'] . '/' . $name);
+
+    if (!$path || strpos($path, $app['upload_folder']) !== 0) {
+        throw new \Exception('File not found');
+    }
+
+    return new BinaryFileResponse($path);
+});
+
+
 
 $app->run();
